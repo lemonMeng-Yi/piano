4f2150b9f8a8ae55daa2f8a6b38548d35e5394d1
lemon <2154212990@qq.com>
2026-01-06 14:01:51 +0800

    [add] 添加启动页

--- app/src/main/java/com/example/piano/MainActivity.kt
+++ app/src/main/java/com/example/piano/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -34,6 +35,9 @@ import com.example.pianotutor.ui.screens.ProgressPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 安装Splash Screen
+        installSplashScreen()
+        
         super.onCreate(savedInstanceState)
         setContent {
             PianoTheme {
